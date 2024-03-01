301a8ddfcb8e06b8b31fb6c6c45f2b2bbdb11701
ch1pset <145881992+user52Real@users.noreply.github.com>
2024-03-01 16:11:25 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "images.pexels.com",
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
